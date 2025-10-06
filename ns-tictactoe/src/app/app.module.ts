438e4d128846fc83520a71633c1d1cf32564caec
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-10-06 21:04:55 +0530

    Added game Component

--- ns-tictactoe/src/app/app.module.ts
+++ ns-tictactoe/src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
-import { NativeScriptModule } from '@nativescript/angular'
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "@nativescript/angular";
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { GameComponent } from "./game/game.component";
+ 
 
 @NgModule({
-  bootstrap: [AppComponent],
+  bootstrap: [GameComponent],
   imports: [NativeScriptModule, AppRoutingModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, GameComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
