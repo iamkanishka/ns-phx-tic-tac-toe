8a38572aedf2f04f24214c64aea97be07dcec659
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-10-10 20:57:14 +0530

    GameChannel module

--- phx_tictactoe/lib/tic_tac_toe_web/channels/game_channel.ex
+++ phx_tictactoe/lib/tic_tac_toe_web/channels/game_channel.ex
@@ -1,100 +1,97 @@
-# lib/tic_tac_toe_web/channels/game_channel.ex
 defmodule TicTacToeWeb.GameChannel do
-  use TicTacToeWeb, :channel
-  alias TicTacToe.Game.GameContext, as: Game
-  alias TicTacToe.Guardian
+  use Phoenix.Channel
+  alias TicTacToe.{Repo, Games.Game, Games.GameServer, GameSupervisor}
 
   def join("game:" <> game_id, _params, socket) do
-    case Guardian.resource_from_token(socket.assigns.token) do
-      {:ok, user} ->
-        case Game.get_game_with_moves(game_id) do
-          nil ->
-            {:error, %{reason: "game not found"}}
-          game ->
-            if user_can_join_game?(game, user) do
-              socket = socket
-                |> assign(:game_id, game_id)
-                |> assign(:user_id, user.id)
+    case Repo.get(Game, game_id) do
+      nil ->
+        {:error, %{reason: "game_not_found"}}
 
-              {:ok, game_state(game), socket}
-            else
-              {:error, %{reason: "unauthorized"}}
-            end
-        end
-      _ ->
-        {:error, %{reason: "unauthorized"}}
+      _game ->
+        send(self(), :after_join)
+        {:ok, assign(socket, :game_id, game_id)}
     end
   end
 
-  def handle_in("join_game", %{"symbol" => symbol}, socket) when symbol in ~w(X O) do
-    case Game.join_game(socket.assigns.game_id, socket.assigns.user_id, symbol) do
+  def handle_info(:after_join, socket) do
+    game_id = socket.assigns.game_id
+
+    case Registry.lookup(TicTacToe.GameRegistry, game_id) do
+      [] -> GameSupervisor.start_game(game_id)
+      _ -> :ok
+    end
+
+    Phoenix.PubSub.subscribe(TicTacToe.PubSub, "game:#{game_id}")
+
+    {:ok, game} = GameServer.get_state(game_id)
+    push(socket, "game_state", format_game_state(game))
+
+    {:noreply, socket}
+  end
+
+  def handle_in("join_game", _payload, socket) do
+    game_id = socket.assigns.game_id
+    player_id = socket.assigns.player_id
+    player_name = socket.assigns.player_name
+
+    case GameServer.join_game(game_id, player_id, player_name) do
       {:ok, game} ->
-        broadcast_game_state(game)
-        {:reply, {:ok, game_state(game)}, socket}
+        {:reply, {:ok, format_game_state(game)}, socket}
 
-      {:error, changeset} ->
-        {:reply, {:error, %{error: "failed to join game"}}, socket}
+      {:error, reason} ->
+        {:reply, {:error, %{reason: Atom.to_string(reason)}}, socket}
     end
   end
 
-  def handle_in("make_move", %{"position" => position}, socket) do
-    case Game.make_move(socket.assigns.game_id, socket.assigns.user_id, position) do
+  def handle_in("make_move", %{"index" => index}, socket) do
+    game_id = socket.assigns.game_id
+    player_id = socket.assigns.player_id
+
+    case GameServer.make_move(game_id, player_id, index) do
       {:ok, game} ->
-        broadcast_game_state(game)
-        {:reply, {:ok, game_state(game)}, socket}
+        {:reply, {:ok, format_game_state(game)}, socket}
 
-      {:error, :invalid_move} ->
-        {:reply, {:error, %{error: "invalid move"}}, socket}
-
-      {:error, _} ->
-        {:reply, {:error, %{error: "move failed"}}, socket}
+      {:error, reason} ->
+        {:reply, {:error, %{reason: Atom.to_string(reason)}}, socket}
     end
   end
 
-  def handle_in("reset_game", _params, socket) do
-    # Implement game reset logic
-    {:reply, {:ok, %{}}, socket}
+  def handle_info({:game_started, game}, socket) do
+    push(socket, "game_started", format_game_state(game))
+    {:noreply, socket}
   end
 
-  defp user_can_join_game?(game, user) do
-    game.status == "waiting" ||
-    game.player_x_id == user.id ||
-    game.player_o_id == user.id
+  def handle_info({:move_made, game, player_id, position}, socket) do
+    push(socket, "move_made", Map.merge(format_game_state(game), %{
+      last_move: %{
+        player_id: player_id,
+        position: position
+      }
+    }))
+    {:noreply, socket}
   end
 
-  defp game_state(game) do
+  defp format_game_state(game) do
     %{
       id: game.id,
-      board: game.board,
+      cells: game.board,
+      state: game.status,
       current_player: game.current_player,
-      status: game.status,
       winner: game.winner,
       winning_line: game.winning_line,
-      player_x: user_info(game.player_x),
-      player_o: user_info(game.player_o),
-      moves: Enum.map(game.moves, &move_info/1)
+      player_x: %{
+        id: game.player_x_id,
+        name: game.player_x_name
+      },
+      player_o: if game.player_o_id do
+        %{
+          id: game.player_o_id,
+          name: game.player_o_name
+        }
+      else
+        nil
+      end,
+      moves_count: game.moves_count
     }
-  end
-
-  defp user_info(nil), do: nil
-  defp user_info(user) do
-    %{
-      id: user.id,
-      name: user.name,
-      avatar_url: user.avatar_url
-    }
-  end
-
-  defp move_info(move) do
-    %{
-      position: move.position,
-      symbol: move.symbol,
-      user_id: move.user_id,
-      inserted_at: move.inserted_at
-    }
-  end
-
-  defp broadcast_game_state(game) do
-    TicTacToeWeb.Endpoint.broadcast!("game:#{game.id}", "game_updated", game_state(game))
   end
 end
