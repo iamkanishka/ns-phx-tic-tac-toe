06aea7aabd9c507ec309e2beccb7e8e0d97d5bfa
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-10-11 23:28:55 +0530

    Added CORS plug

--- phx_tictactoe/lib/tic_tac_toe_web/endpoint.ex
+++ phx_tictactoe/lib/tic_tac_toe_web/endpoint.ex
@@ -5,10 +5,6 @@ defmodule TicTacToeWeb.Endpoint do
     websocket: [timeout: 45_000],
     longpoll: false
 
-
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
   @session_options [
     store: :cookie,
     key: "_tic_tac_toe_key",
@@ -20,18 +16,12 @@ defmodule TicTacToeWeb.Endpoint do
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
   plug Plug.Static,
     at: "/",
     from: :tic_tac_toe,
     gzip: false,
     only: TicTacToeWeb.static_paths()
 
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
   if code_reloading? do
     plug Phoenix.CodeReloader
     plug Phoenix.Ecto.CheckRepoStatus, otp_app: :tic_tac_toe
@@ -51,6 +41,10 @@ defmodule TicTacToeWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
+
+  # ✅ ADD YOUR CUSTOM CORS PLUG *before* session and router plugs
+  plug TicTacToeWeb.Plugs.CORS
+
   plug Plug.Session, @session_options
   plug TicTacToeWeb.Router
 end
