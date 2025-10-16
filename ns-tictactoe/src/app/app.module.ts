ff8bfd9688b146132886105fd907c9b8a280af4b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-10-16 21:03:58 +0530

    Added NativeScriptFormsModule

--- ns-tictactoe/src/app/app.module.ts
+++ ns-tictactoe/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "@nativescript/angular";
-
+import { NativeScriptFormsModule } from '@nativescript/angular';
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-
+import { FormsModule } from '@angular/forms';
 import { GameComponent } from "./game/game.component";
- 
+
 
 @NgModule({
   bootstrap: [GameComponent],
-  imports: [NativeScriptModule, AppRoutingModule],
+  imports: [NativeScriptModule, AppRoutingModule, NativeScriptFormsModule],
   declarations: [AppComponent, GameComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
