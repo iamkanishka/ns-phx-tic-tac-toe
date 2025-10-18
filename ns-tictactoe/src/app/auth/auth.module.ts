ce231a056dbfcdbbeb346f443d1e9ca72bdbc1da
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-10-18 21:21:05 +0530

    AuthModule

---
+++ ns-tictactoe/src/app/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { AuthRoutingModule } from "./auth-routing.module";
+import { NativeScriptCommonModule } from "@nativescript/angular";
+// import { SignInComponent } from "./sign-in/sign-in.component";
+
+
+@NgModule({
+  declarations: [],
+  imports: [NativeScriptCommonModule, AuthRoutingModule],
+  schemas: [NO_ERRORS_SCHEMA],
+})
+export class AuthModule {}
