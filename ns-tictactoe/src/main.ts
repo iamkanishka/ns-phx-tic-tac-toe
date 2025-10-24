ff169024f8e246adba7b90fbc594b7535a8393a4
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-10-24 21:25:29 +0530

    registerElement GoogleSignInButton

--- ns-tictactoe/src/main.ts
+++ ns-tictactoe/src/main.ts
@@ -1,6 +1,11 @@
 import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
 
 import { AppModule } from './app/app.module';
+import { registerElement } from "@nativescript/angular";
+registerElement(
+  "GoogleSignInButton",
+  () => require("@nativescript/google-signin").GoogleSignInButton
+);
 
 runNativeScriptAngularApp({
   appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
