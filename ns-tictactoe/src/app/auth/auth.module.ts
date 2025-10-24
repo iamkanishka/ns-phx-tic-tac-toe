8b2969c52308932dd12869216b7e781066a5900e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-10-24 21:26:58 +0530

    Added signin comp

--- ns-tictactoe/src/app/auth/auth.module.ts
+++ ns-tictactoe/src/app/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { AuthRoutingModule } from "./auth-routing.module";
 import { NativeScriptCommonModule } from "@nativescript/angular";
-// import { SignInComponent } from "./sign-in/sign-in.component";
+ import { SignInComponent } from "./sign-in/sign-in.component";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SignInComponent],
   imports: [NativeScriptCommonModule, AuthRoutingModule],
   schemas: [NO_ERRORS_SCHEMA],
 })
