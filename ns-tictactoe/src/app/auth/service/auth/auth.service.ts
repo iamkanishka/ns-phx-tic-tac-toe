c4e6781dacb23a50183d189484c8c3739a776697
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-10-27 21:04:24 +0530

    Updated Auth Service

--- ns-tictactoe/src/app/auth/service/auth/auth.service.ts
+++ ns-tictactoe/src/app/auth/service/auth/auth.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { GoogleSignin } from "@nativescript/google-signin";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
+  const;
   constructor(private http: HttpClient) {}
 
-  getGoogleSignInUserDetailsAPI(accessToken:string): Observable<any> {
-    return this.http.get("https://www.googleapis.com/oauth2/v3/userinfo?access_token=" + accessToken);
-   }
+
+  getGoogleSignInUserDetailsAPI(): Observable<any> {
+    const accessToken: string = GoogleSignin.getCurrentUser()["_accessToken"];
+
+    return this.http.get(
+      "https://www.googleapis.com/oauth2/v3/userinfo?access_token=" +
+        accessToken
+    );
+  }
+
+  public getCurrentUser(): Promise<any> {
+    return Promise.resolve(GoogleSignin.getCurrentUser());
+  }
 }
